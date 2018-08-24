8f83124bf8601ac3a5f54fbe0c94449aa81ccf4f
sami peralahti <sami.peralahti@vainu.io>
2018-08-24 15:25:58 -0700

    extracting many data points

---
+++ craigslist/craigslist/spiders/jobs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    allowed_domains = ['newyork.craigslist.org/search/egr']
+    start_urls = ['https://newyork.craigslist.org/search/egr']
+
+    def parse(self, response):
+        jobs = response.xpath('//p[@class="result-info"]')
+        for job in jobs:
+            title = job.xpath('a/text()').extract_first()
+            address = job.xpath('span[@class="result-meta"]/span[@class="result-hood"]/text()').extract_first("")[2:-1]
+            relative_url = job.xpath('a/@href').extract_first()
+            absolute_url = response.urljoin(relative_url)
+
+            yield{'URL':absolute_url, 'Title':title, 'Address':address}
