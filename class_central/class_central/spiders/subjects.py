663d5e9dfee6d800827c21b26365fc0706ebaf91
sami peralahti <sami.peralahti@vainu.io>
2018-08-25 19:14:35 -0700

    added logic to scrape just one subject or all

--- class_central/class_central/spiders/subjects.py
+++ class_central/class_central/spiders/subjects.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
-
+from scrapy.http import Request
 
 class SubjectsSpider(Spider):
     name = 'subjects'
@@ -12,6 +12,13 @@ class SubjectsSpider(Spider):
 
     def parse(self, response):
         if self.subject:
-            print("True")
+            subject_url = response.xpath('//*[contains(@title, ' + self.subject + ')]/@href').extract_first()
+            yield Request(response.urljoin(subject_url), callback=self.parse_subject)
         else:
-            print("False")
+            self.logger.info("Scraping all subjects")
+            subjects = response.xpath('//*[@class="head-3 large-up-head-2 text--bold block"]/../@href').extract()
+            for subject in subjects:
+                yield Request(response.urljoin(subject), callback= self.parse_subject)
+
+    def parse_subject(self, response):
+        pass
