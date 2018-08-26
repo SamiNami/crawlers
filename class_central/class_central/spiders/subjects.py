3af738bbc01c163e98f6ed1195dc8c74a170ae3c
sami peralahti <sami.peralahti@vainu.io>
2018-08-25 21:38:28 -0700

    scraping some basic data works

--- class_central/class_central/spiders/subjects.py
+++ class_central/class_central/spiders/subjects.py
@@ -21,4 +21,19 @@ class SubjectsSpider(Spider):
                 yield Request(response.urljoin(subject), callback= self.parse_subject)
 
     def parse_subject(self, response):
-        pass
+        subject_name = response.xpath('//title/text()').extract_first()
+        subject_name = subject_name.split(' | ')
+        subject_name = subject_name[0]
+
+        courses = response.xpath('//*[@class="text--charcoal text-2 medium-up-text-1 block course-name"]')
+        for course in courses:
+            course_name = course.xpath('.//@title').extract_first()
+            course_url = course.xpath('.//@href').extract_first()
+            absolute_course_url = response.urljoin(course_url)
+
+            yield {
+                "subject_name": subject_name,
+                "course_name": course_name,
+                "absolute_course_url": absolute_course_url,
+            }
+    
